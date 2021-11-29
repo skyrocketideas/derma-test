a76313da5bea2a52dbb1dcd8b522ae783c1665a2
graeme <grae0026@stud.kea.dk>
2021-11-29 19:31:03 +0100

    fix: desktop nav layout

---
+++ main.js
@@ -0,0 +1,7 @@
+let navMain = document.getElementById("nav-js");
+
+let navToggle = document.getElementById("nav-toggle-js");
+
+navToggle.addEventListener("click", function () {
+	navMain.classList.toggle("active");
+});
